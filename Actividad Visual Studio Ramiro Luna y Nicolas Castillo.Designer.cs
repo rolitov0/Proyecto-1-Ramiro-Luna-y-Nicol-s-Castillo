3c9aa604904ac112c74d9757fe3fface4b9e86a6
sanpinto <sanpingo@gmail.com>
2025-06-23 10:58:48 -0300

    Primera Versión

---
+++ Actividad Visual Studio Ramiro Luna y Nicolas Castillo.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FrmHolaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.lblHolaMundo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.Location = new System.Drawing.Point(294, 127);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(234, 79);
+            this.btnSaludar.TabIndex = 0;
+            this.btnSaludar.Text = "Saludar";
+            this.btnSaludar.UseVisualStyleBackColor = true;
+            this.btnSaludar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHolaMundo
+            // 
+            this.lblHolaMundo.AutoSize = true;
+            this.lblHolaMundo.Location = new System.Drawing.Point(362, 209);
+            this.lblHolaMundo.Name = "lblHolaMundo";
+            this.lblHolaMundo.Size = new System.Drawing.Size(91, 13);
+            this.lblHolaMundo.TabIndex = 1;
+            this.lblHolaMundo.Text = "                            ";
+            this.lblHolaMundo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHolaMundo);
+            this.Controls.Add(this.btnSaludar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaludar;
+        private System.Windows.Forms.Label lblHolaMundo;
+    }
+}
+
